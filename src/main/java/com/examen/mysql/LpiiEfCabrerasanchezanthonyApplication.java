e3cb96ba04e33b65a79c2d430bbbfbb112402add
thony1014 <thony@gmail.com>
2024-10-23 22:08:48 -0500

    inicio de proyecto ef

---
+++ src/main/java/com/examen/mysql/LpiiEfCabrerasanchezanthonyApplication.java
@@ -0,0 +1,13 @@
+package com.examen.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiEfCabrerasanchezanthonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiEfCabrerasanchezanthonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/mysql/LpiiEfCabrerasanchezanthonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiiEfCabrerasanchezanthonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
